c0c46f41761d6f8dfa1137b5c703e6bb450aa0a9
Guillem Cantallops Ramis <guillem@gmail.com>
2016-08-10 23:33:01 +0200

    better python version

--- python/grepc.py
+++ python/grepc.py
@@ -6,7 +6,7 @@ t0 = time.time()
 counter = 0
 with open(sys.argv[2]) as f:
 	for line in f:
-		if re.search(sys.argv[1], line):
+		if sys.argv[1] in line:
 			counter += 1
 print counter
 t1 = time.time()
